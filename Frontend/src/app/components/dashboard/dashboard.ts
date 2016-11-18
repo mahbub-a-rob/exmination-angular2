b8e84bd1d72bae1ba74621d0fe3ea33c612504b7
wanchaloem laoket <wanchaloem.laokeut@googlemail.com>
2016-11-18 10:13:53 +0700

    clean project

--- Frontend/src/app/components/dashboard/dashboard.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-    selector: 'app-dashboard',
-    templateUrl: 'dashboard.html',
-    styleUrls: ['dashboard.less']
-})
-export class DashboardComponent {
-
-}
