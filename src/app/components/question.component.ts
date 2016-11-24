c33a66e693a67122f5e6e569a160d85cd020cf12
wanchaloem laoket <wanchaloem.laokeut@googlemail.com>
2016-11-24 09:59:55 +0700

    update question

--- src/app/components/question.component.ts
+++ src/app/components/question.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Url } from '../factories/url.factory';
 import { Router, ActivatedRoute } from '@angular/router';
 import { QuestionService, QuestionModel } from '../services/question.service';
@@ -21,24 +21,32 @@ export class QuestionComponent {
 		private examinationService: ExaminationService) {
 		// create validator form
 		this.form = build.group({
-
+			sequence: [],
+			name: [],
+			detail: [],
+			examination_id: []
 		});
 		// get subject id 
 		route.params.forEach(param => {
 			if (isNaN(param['id'])) { this.router.navigate([Url.Examination]); return; }
 			this.subjectID = param['id'];
 			// get questions
-			this.service.details().subscribe(res => this.questions = res);
+			this.service.details().subscribe(res => {
+				this.questions = res;
+				this.form.controls['sequence'].setValue((this.questions.length + 1));
+			});
 			// get subjects
 			this.examinationService.detailSubject(this.subjectID).subscribe(res => {
 				if (res == null) return;
 				this.examination = res.examination;
 				this.subject = res.subject;
+				this.form.controls['examination_id'].setValue(this.examination.id);
 			});
 		});
 	}
 
 	Url = Url;
+	@ViewChild('sequence') sequence: HTMLInputElement;
 	form: FormGroup;
 	subjectID: any;
 	questions: Array<QuestionModel> = [];
@@ -46,4 +54,11 @@ export class QuestionComponent {
 	subject: SubjectModel = new SubjectModel();
 	model: QuestionModel;
 	answerArray: string[] = ['ก', 'ข', 'ค', 'ง'];
+
+	onSubmit() {
+		if (this.form.valid) {
+			this.model = this.form.value;
+			console.log(this.model, this.sequence);
+		}
+	}
 }
